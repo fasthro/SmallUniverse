7d73824f46d3d4e222a2ea7e653ff41e2fd67ee1
fasthro <chenchaoflying@outlook.com>
2019-01-21 20:51:23 +0800

    修改模型

--- Client/Assets/Scripts/Scene/BattleScene.cs
+++ Client/Assets/Scripts/Scene/BattleScene.cs
@@ -16,7 +16,7 @@ namespace SmallUniverse.Scene
 
             panelMgr.CreatePanel(UI.PanelName.HudPanel);
 
-            levelMgr.InitLevel(100001, 10002);
+            levelMgr.InitLevel(100001, 10003);
         }
 
         public override void OnLeaveScenel(SceneType level, Action action = null)
