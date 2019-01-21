f1e09597c4d36ce04fd8dc5fa4b333d09911d057
fasthro <chenchaoflying@outlook.com>
2019-01-21 20:36:37 +0800

    xml to csv, 修改模型

--- Client/Assets/XmlToCSVEditor/Scripts/XmlData.cs
+++ Client/Assets/XmlToCSVEditor/Scripts/XmlData.cs
@@ -2,25 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SmallUniverse.GameEditor.XmlEditor
-{
-    public class XmlData
-    {
+namespace SmallUniverse.GameEditor.XmlEditor {
+    public class XmlData {
         public string[] values;
 
-        public string ToCSV()
-        {
+        public string ToCSV () {
             string csv = string.Empty;
 
-            for (int i = 0; i < values.Length; i++)
-            {
-                if (string.IsNullOrEmpty(csv))
-                {
+            for (int i = 0; i < values.Length; i++) {
+                if (string.IsNullOrEmpty (csv)) {
                     csv = values[i];
-                }
-                else
-                {
-                    csv = string.Format("{0},{1}", csv, values[i].TrimEnd('\r'));
+                } else {
+                    if (values[i] != null) {
+                        csv = string.Format ("{0},{1}", csv, values[i].TrimEnd ('\r'));
+                    } else {
+                        Debug.LogError("xmlData ToCSV Error! " + values[i].ToString());
+                    }
                 }
             }
             return csv + "\n";
--- Client/Assets/XmlToCSVEditor/Scripts/XmlWorksheet.cs
+++ Client/Assets/XmlToCSVEditor/Scripts/XmlWorksheet.cs
@@ -1,14 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using System.Security;
 using System.IO;
-using System;
+using System.Security;
+using UnityEngine;
 
-namespace SmallUniverse.GameEditor.XmlEditor
-{
-    public class XmlWorksheet
-    {
+namespace SmallUniverse.GameEditor.XmlEditor {
+    public class XmlWorksheet {
         // 数据类型字典
         private static Dictionary<string, string> typeDic;
 
@@ -26,11 +24,11 @@ namespace SmallUniverse.GameEditor.XmlEditor
         public bool isNull;
 
         private string m_xmlPath;
-        public XmlWorksheet(string xmlPath, SecurityElement tableElement)
-        {
+        public XmlWorksheet (string xmlPath, SecurityElement tableElement) {
+            Debug.Log ("CSV -> " + xmlPath);
             m_xmlPath = xmlPath;
-            rowCount = int.Parse(tableElement.Attribute("ss:ExpandedRowCount"));
-            columnCount = int.Parse(tableElement.Attribute("ss:ExpandedColumnCount"));
+            rowCount = int.Parse (tableElement.Attribute ("ss:ExpandedRowCount"));
+            columnCount = int.Parse (tableElement.Attribute ("ss:ExpandedColumnCount"));
 
             isNull = rowCount > 3;
 
@@ -39,49 +37,40 @@ namespace SmallUniverse.GameEditor.XmlEditor
 
             rowElements = new SecurityElement[rowCount];
             int index = 0;
-            foreach (SecurityElement child in tableElement.Children)
-            {
-                if (child.Tag.Equals("Row"))
-                {
+            foreach (SecurityElement child in tableElement.Children) {
+                if (child.Tag.Equals ("Row")) {
                     rowElements[index] = child;
                     index++;
                 }
             }
-            
-            describeData = CreateXmlData(rowElements[0]);
-            typeData = CreateXmlData(rowElements[1]);
-            keyData = CreateXmlData(rowElements[2]);
+
+            describeData = CreateXmlData (rowElements[0]);
+            typeData = CreateXmlData (rowElements[1]);
+            keyData = CreateXmlData (rowElements[2]);
 
             xmlDatas = new XmlData[rowCount - 3];
-            for (int i = 3; i < rowCount; i++)
-            {
-                xmlDatas[i - 3] = CreateXmlData(rowElements[i]);
+            for (int i = 3; i < rowCount; i++) {
+                xmlDatas[i - 3] = CreateXmlData (rowElements[i]);
             }
         }
 
-        private XmlData CreateXmlData(SecurityElement rowElement)
-        {
-            XmlData xmlData = new XmlData();
+        private XmlData CreateXmlData (SecurityElement rowElement) {
+            XmlData xmlData = new XmlData ();
             xmlData.values = new string[columnCount];
             int index = 0;
-            foreach (SecurityElement cellChild in rowElement.Children)
-            {
-                if (cellChild.Tag.Equals("Cell"))
-                {
-                    if (cellChild.Children != null)
-                    {
-                        foreach (SecurityElement dataChild in cellChild.Children)
-                        {
-                            if (dataChild.Tag.Equals("Data"))
-                            {
-                                xmlData.values[index] = dataChild.Text;
+            foreach (SecurityElement cellChild in rowElement.Children) {
+                if (cellChild.Tag.Equals ("Cell")) {
+                    if (cellChild.Children != null) {
+                        foreach (SecurityElement dataChild in cellChild.Children) {
+                            if (dataChild.Tag.Equals ("Data") || dataChild.Tag.Equals ("ss:Data")) {
+                                string text = string.Empty;
+                                GetText (dataChild, out text);
+                                xmlData.values[index] = text;
                             }
                         }
                         index++;
-                    }
-                    else
-                    {
-                        throw new Exception("excel have empty data grid! " + m_xmlPath);
+                    } else {
+                        throw new Exception ("excel have empty data grid! " + m_xmlPath);
                     }
 
                 }
@@ -89,64 +78,68 @@ namespace SmallUniverse.GameEditor.XmlEditor
             return xmlData;
         }
 
-        public string ToCSV()
-        {
+        private void GetText (SecurityElement element, out string text) {
+            
+            text = string.Empty;
+
+            if (element.Children != null) {
+                foreach (SecurityElement child in element.Children) {
+                    GetText (child, out text);
+                }
+            } else {
+                text = element.Text;
+            }
+        }
+
+        public string ToCSV () {
             string csv = string.Empty;
-            for (int i = 0; i < xmlDatas.Length; i++)
-            {
-                csv += xmlDatas[i].ToCSV();
+            for (int i = 0; i < xmlDatas.Length; i++) {
+                csv += xmlDatas[i].ToCSV ();
             }
             return csv;
         }
 
-        public string ToCode(string className)
-        {
+        public string ToCode (string className) {
             string vars = string.Empty;
-            for (int i = 0; i < columnCount; i++)
-            {
-                vars += GetVarStr(i);
+            for (int i = 0; i < columnCount; i++) {
+                vars += GetVarStr (i);
             }
-            vars = vars.TrimEnd('\n');
-            
-            string template = GetCodeTemplate();
-            template = template.Replace("[CLASS_NAME]", className);
-            template = template.Replace("[VARS_CODE]", vars);
+            vars = vars.TrimEnd ('\n');
+
+            string template = GetCodeTemplate ();
+            template = template.Replace ("[CLASS_NAME]", className);
+            template = template.Replace ("[VARS_CODE]", vars);
 
             return template;
         }
 
-        private string GetVarStr(int index)
-        {
-            string content = string.Format("        // {0}\n", describeData.values[index]);
-            content += string.Format("        public {0} {1};\n\n", GetTypeStr(typeData.values[index]), keyData.values[index]);
+        private string GetVarStr (int index) {
+            string content = string.Format ("        // {0}\n", describeData.values[index]);
+            content += string.Format ("        public {0} {1};\n\n", GetTypeStr (typeData.values[index]), keyData.values[index]);
             return content;
         }
 
-        public static string GetTypeStr(string type)
-        {
-            if (typeDic == null)
-            {
-                typeDic = new Dictionary<string, string>();
-                typeDic.Add("int", "int");
-                typeDic.Add("float", "float");
-                typeDic.Add("string", "string");
-                typeDic.Add("Vector2", "Vector2");
-                typeDic.Add("Vector3", "Vector3");
-                typeDic.Add("arrayInt", "int[]");
-                typeDic.Add("arrayFloat", "float[]");
-                typeDic.Add("arrayString", "string[]");
+        public static string GetTypeStr (string type) {
+            if (typeDic == null) {
+                typeDic = new Dictionary<string, string> ();
+                typeDic.Add ("int", "int");
+                typeDic.Add ("float", "float");
+                typeDic.Add ("string", "string");
+                typeDic.Add ("Vector2", "Vector2");
+                typeDic.Add ("Vector3", "Vector3");
+                typeDic.Add ("arrayInt", "int[]");
+                typeDic.Add ("arrayFloat", "float[]");
+                typeDic.Add ("arrayString", "string[]");
             }
             string ts = string.Empty;
-            if (typeDic.TryGetValue(type, out ts))
-            {
+            if (typeDic.TryGetValue (type, out ts)) {
                 return ts;
             }
             return ts;
         }
 
-        public static string GetCodeTemplate()
-        {
-            string template = File.ReadAllText(Path.Combine(Application.dataPath, "XmlToCSVEditor/CodeTemplate.txt"));
+        public static string GetCodeTemplate () {
+            string template = File.ReadAllText (Path.Combine (Application.dataPath, "XmlToCSVEditor/CodeTemplate.txt"));
             return template;
         }
     }
